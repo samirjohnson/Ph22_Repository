d02f37e468f64950ac844a4b72bfe6969fb02c3b
samirjohnson <56701692+samirjohnson@users.noreply.github.com>
2020-04-17 13:55:19 -0700

    Update KeplerianOrbit.py

--- KeplerianOrbit.py
+++ KeplerianOrbit.py
@@ -13,18 +13,17 @@ a = 2.34186 * 3 * 10 ** 8; e = 0.617139; T = 27906.98161
 
 eq = lambda C,t: T / (2 * math.pi) * (C - e * math.sin(C)) - t
 deq = lambda C: T / (2 * math.pi) * (1 - e * math.cos(C))
-x = lambda C: a * (math.cos(C) - e)
-y = lambda C: a * math.sqrt(1 - e ** 2) * math.sin(C)
+X = lambda C: a * (math.cos(C) - e)
+Y = lambda C: a * math.sqrt(1 - e ** 2) * math.sin(C)
 
-GUESS = lambda t: 2 * math.pi * t / (T * (1 - e)); tol = 10 ** (-6)
+GUESS = lambda t: 2 * math.pi * t / T; tol = 10 ** (-6)
 
-tList = np.linspace(0,1000,100001)
+tList = np.linspace(0,T,int(T)+1)
 xList = []; yList = [];
 for t in tList:
     C = NewtonRaphson(eq, deq, GUESS(t), t, tol)
-    xList.append(x(C))
-    yList.append(y(C))
-
+    xList.append(X(C))
+    yList.append(Y(C))
 
 plt.plot(xList, yList)
 plt.title("Elliptical Keplerian Orbit")
